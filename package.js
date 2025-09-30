0cb7b895b756d22f7b01109d6703ec02510a0857
Alexis-Naonext <alexis@naonext.com>
2025-09-30 15:51:25 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,3 +1,13 @@
-"scripts": {
-  "start": "node index.js"
+{
+  "name": "mon-api-rest",
+  "version": "1.0.0",
+  "description": "Une API REST simple avec Express",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "keywords": ["api", "express", "nodejs", "rest"],
+  "dependencies": {
+    "express": "^4.18.2"
+  }
 }
