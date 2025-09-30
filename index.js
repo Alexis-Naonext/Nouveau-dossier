0b69bca291e025200c930ba2d98308761940ba69
Alexis-Naonext <alexis@naonext.com>
2025-09-30 16:14:49 +0200

    change for middleware

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/api/salut', (req, res) => {
-  res.json({ message: 'Salut Alexis !' });
-});
+// app.get('/api/salut', (req, res) => {
+//   res.json({ message: 'Salut Alexis !' });
+// });
 
-app.get('/api/bye', (req, res) => {
-  res.json({ message: 'bye Alexis !' });
+// app.get('/api/bye', (req, res) => {
+//   res.json({ message: 'bye Alexis !' });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Serveur lancé sur le port ${port}`);
+// });
+
+
+// Middleware qui intercepte toutes les requêtes
+app.use((req, res) => {
+  res.json({
+    message: 'Requête reçue',
+    methode: req.method,
+    url: req.originalUrl,
+    corps: req.body
+  });
 });
 
 app.listen(port, () => {
