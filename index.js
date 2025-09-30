70cccff9caa67f5ce371eb14f926f645abf90125
Alexis-Naonext <alexis@naonext.com>
2025-09-30 16:36:27 +0200

    ajout des requette gsm et api

--- index.js
+++ index.js
@@ -16,17 +16,50 @@ app.use(express.json());
 //   console.log(`Serveur lancé sur le port ${port}`);
 // });
 
+app.get("/GSM/getdatetime", (req, res) => {
+  const currentDate = new Date();
+  res.json({ datetime: currentDate.toISOString() });
+});
 
-// Middleware qui intercepte toutes les requêtes
-app.use((req, res) => {
-  res.json({
-    message: 'Requête reçue',
-    methode: req.method,
-    url: req.originalUrl,
-    corps: req.body
-  });
+app.get("/GSM/getpublickey", (req, res) => {
+  const public = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzV7b4t3mFv6+X9y5jv1b\nqZl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Z\nl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl5H3F5Zl\n-----END PUBLIC KEY-----";
+  res.json({ publicKey: public });
+});
+
+app.post("/GSM/injectdata", (req, res) => {
+  const data = req.body;
+  console.log("Données reçues :", data);
+  res.json({ message: "Données reçues avec succès", receivedData: data });
+});
+
+app.post("/api/testPOST", (req, res) => {
+  const data = req.body;
+  console.log("Données reçues :", data);
+  res.json({ message: "Données reçues avec succès", receivedData: data });
+});
+
+app.get("/api/getdatetime", (req, res) => {
+  const currentDate = new Date();
+  res.json({ datetime: currentDate.toISOString() });
 });
 
 app.listen(port, () => {
   console.log(`Serveur lancé sur le port ${port}`);
-});
+}); 
+
+
+
+
+// Middleware qui intercepte toutes les requêtes
+    // app.use((req, res) => {
+    //   res.json({
+    //     message: 'Requête reçue',
+    //     methode: req.method,
+    //     url: req.originalUrl,
+    //     corps: req.body
+    //   });
+    // });
+
+    // app.listen(port, () => {
+    //   console.log(`Serveur lancé sur le port ${port}`);
+    // });
