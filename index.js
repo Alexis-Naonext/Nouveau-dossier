5282002463c033d5cf779818a925715dce5edd81
Alexis-Naonext <alexis@naonext.com>
2025-10-01 10:35:28 +0200

    url webpage and json

--- index.js
+++ index.js
@@ -41,23 +41,29 @@ app.use(express.json());
 
 
 // Middleware qui intercepte toutes les requêtes
-app.use((req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Requête reçue</title>
-      </head>
-      <body>
-        <h1>Requête reçue</h1>
-        <p><strong>Méthode :</strong> ${req.method}</p>
-        <p><strong>URL :</strong> ${req.originalUrl}</p>
-        <p><strong>Corps :</strong> ${JSON.stringify(req.body)}</p>
-      </body>
-    </html>
-  `);
-});
+    app.use((req, res) => {
+      res.send(`
+        <html>
+          <head>
+            <title>Requête reçue</title>
+          </head>
+          <body>
+            <h1>Requête reçue</h1>
+            <p><strong>Méthode :</strong> ${req.method}</p>
+            <p><strong>URL :</strong> ${req.originalUrl}</p>
+            <p><strong>Corps :</strong> ${JSON.stringify(req.body)}</p>
+          </body>
+        </html>
+      `);
 
+      res.json({
+        message: 'Requête reçue',
+        methode: req.method,
+        url: req.originalUrl,
+        corps: req.body
+      });
+    });
 
-app.listen(port, () => {
-  console.log(`Serveur lancé sur le port ${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Serveur lancé sur le port ${port}`);
+    });
